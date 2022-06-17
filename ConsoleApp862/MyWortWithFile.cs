f1bcc578d0abd9d292cb359ba3f4ff15d7bb9da1
mispansk <mispansk@gmail.com>
2022-06-17 13:52:31 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp862/MyWortWithFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp862
+{
+    /// <summary>
+    /// класс работы с файлами
+    /// </summary>
+    static public class MyWorkWithFile
+    {
+        /// <summary>
+        /// считаем объем всех файлов в указанной директории (и в глубине дирректории)
+        /// </summary>
+        /// <param name="info"> заданная директория </param>
+        /// <returns> общий объем файлов </returns>
+        static public long FileSize(DirectoryInfo info)
+        {
+            long total = 0;
+            try
+            {
+                foreach (FileInfo file in info.GetFiles())
+                {
+                    Console.WriteLine("ОбЪем файла {0} : {1}", file, file.Length);
+                    total += +file.Length;
+                }
+
+                foreach (DirectoryInfo dir in info.GetDirectories())
+                {
+                    total += FileSize(dir);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Процесс не удался: {0}", e.Message);
+            }
+
+            return total;
+        }
+    }
+}
---
+++ ConsoleApp862/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp862
+{
+    // Задание 8.6.2 программа, которая считает размер папки на диске
+    class Program
+    {                   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dirName = new DirectoryInfo(@"C:\Work\skillFactori\123"); // Прописываем путь к директории 
+            long tottal = 0;
+
+            if (dirName.Exists)
+            {
+                 tottal = MyWorkWithFile.FileSize(dirName);
+            }
+            else
+            {
+                Console.WriteLine("Некорректно указан путь");
+            }
+            Console.WriteLine("Oбъем файлов {0}", tottal);
+            Console.ReadKey();
+        }
+    }
+}
